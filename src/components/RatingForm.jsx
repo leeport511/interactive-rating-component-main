40bde533edea9cea2cefa5d9f06b205c4e0b4b3f
leeport511 <leonardoandres.portillo@gmail.com>
2023-02-27 20:16:21 -0300

    added hover and active effects on RatingForm

--- src/components/RatingForm.jsx
+++ src/components/RatingForm.jsx
@@ -6,16 +6,16 @@ const arrayRating = [1, 2, 3, 4, 5];
 export const RatingForm = () => {
     return (
         <div className="container flex h-screen items-center justify-center bg-very-dark-blue">
-            <div className="m-auto w-[85%]">
+            <div className="m-auto w-[80%]">
                 <section className="rounded-2xl bg-dark-blue p-6">
                     <div className="mb-7 flex h-[50px] w-[50px] items-center justify-center rounded-full bg-gray-800">
                         <StarIcon />
                     </div>
                     <div className="mb-7">
-                        <h2 className="mb-3 text-3xl text-white">
+                        <h2 className="mb-3 text-3xl text-white font-bold">
                             How did we do?
                         </h2>
-                        <p className="text-[15px] text-light-grey">
+                        <p className="text-left text-[15px] text-light-grey">
                             Please let us know how we did with your support
                             request. All feedback is appreciated to help us
                             improve our offering!
@@ -25,9 +25,9 @@ export const RatingForm = () => {
                         <div className="mb-5 flex justify-between ">
                             {arrayRating.map((rating, i) => {
                                 return (
-                                    <div className="mb-3 flex h-[50px] w-[50px] items-center justify-center rounded-full bg-gray-700">
+                                    <div className="mb-3 ">
                                         <button
-                                            className="text-light-grey text-[15px]"
+                                            className="flex h-[50px] w-[50px] items-center justify-center rounded-full bg-gray-700 text-[15px] text-light-grey  hover:bg-medium-grey hover:text-dark-blue active:bg-orange active:text-white"
                                             key={i}
                                         >
                                             {rating}
@@ -38,7 +38,7 @@ export const RatingForm = () => {
                         </div>
                         <button
                             type="submit"
-                            className="w-[100%] rounded-full bg-orange p-4 text-white tracking-[0.35rem]"
+                            className="w-[100%] rounded-full bg-orange p-4 tracking-[0.25rem] text-white active:text-orange active:bg-white font-bold hover:opacity-80"
                         >
                             SUBMIT
                         </button>
